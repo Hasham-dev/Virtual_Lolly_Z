94a5c6225171350016dba956b878a06e92d76299
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-11-08 06:33:07 +0500

    Style.css finalized

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="header">
         <h1 className="header__heading">Virtual lollipop</h1>
-        <p className="header__subheading">because we all know someone who deserves some sugar.</p>
+        <p className="header__subheading">because we all know someone <br/> who deserves some sugar.</p>
     </header>
   )
 }
